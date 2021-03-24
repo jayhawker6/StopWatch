811f7788dacd2767c06572997bf365d83db2a1a9
Andrew Guernsey <344agg21@students.olatheschools.com>
2021-03-24 20:03:17 +0000

    Fixed format errors. Gave it a more robust variable for calculating time.

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ runtime = time.time()
 
 
 def watchui():
-    global watchtime
-    input('Press enter to start the stopwatch')
-    watchtime = time.time()
-    input('Press enter to stop the stopwatch')
+		global watchtime
+		global watchstoptime
+		input('Press enter to start the stopwatch')
+		watchtime = time.time()
+		input('Press enter to stop the stopwatch')
+		watchstoptime = time.time()
 
 
 watchui()
 
 while True:
-    try:
-        response = int(
-            input('''
-		 Time: %s
-
-		 Would you like to restart it?
-		 
-		 1- Yes
-		 2- No
-		 ''' % (time.time() - watchtime)))
-    except ValueError:
-        print('Please use one of the provided answers!')
-    if response == 1:
-        watchui()
-        continue
-    elif response == 2:
-        quit("--- %s of runtime ---" % (time.time() - runtime))
+	try:
+		response = int(input('''
+		Time: %s
+		Would you like to restart it? 
+		1- Yes
+		2- No
+		''' % (watchstoptime - watchtime)))
+	except ValueError:
+		print('Please use one of the provided answers!')
+		continue
+	if response == 1:
+		watchui()
+		continue
+	elif response == 2:
+		quit("--- %s of runtime ---" % (time.time() - runtime))
